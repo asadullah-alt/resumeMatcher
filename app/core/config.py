19658bb99a3a951ab4f519de5ac928462f83bd21
asadullah-alt <asadullahbeg@outlook.com>
2025-12-30 11:55:45 +0500

    modified prompt

--- app/core/config.py
+++ app/core/config.py
@@ -30,7 +30,7 @@ class Settings(BaseSettings):
     #LL_MODEL: Optional[str] = "gemma3:4b"
     LLM_BASE_URL: Optional[str] = "https://openrouter.ai/api/v1"
     #LL_MODEL: Optional[str] = "openai/gpt-oss-safeguard-20b"
-    LLM_MODEL: Optional[str] = "openai/gpt-4o"
+    LL_MODEL: Optional[str] = "openai/gpt-4o"
     EMBEDDING_PROVIDER: Optional[str] = "ollama"
     EMBEDDING_API_KEY: Optional[str] = None
     EMBEDDING_BASE_URL: Optional[str] = None
