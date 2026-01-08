c78c35de21330a516a4545a20dd298544560f0ef
asadullah-alt <asadullahbeg@outlook.com>
2026-01-08 23:50:51 +0500

    Something is up

--- app/prompt/structured_job.py
+++ app/prompt/structured_job.py
@@ -5,7 +5,7 @@ You are a JSON-extraction engine. Convert the following raw job posting text int
 — Ensure any URLs (website, applyLink) conform to URI format.
 — Do not change the structure or key names; output only valid JSON matching the schema.
 - Do not format the response in Markdown or any other format. Just output raw JSON.
-
+- ExtractedKeywords should be a list of all the skills required hard skills or soft skills of any type
 Schema:
 ```json
 {0}
