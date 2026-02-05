7a3790b608f53e68bfa87c1a3416826d7d95318e
asadullah-alt <asadullahbeg@outlook.com>
2026-02-05 18:36:13 +0500

    experimenting with flash

--- app/core/config.py
+++ app/core/config.py
@@ -30,7 +30,7 @@ class Settings(BaseSettings):
     #LL_MODEL: Optional[str] = "mistral-nemo"
     LLM_BASE_URL: Optional[str] = "https://openrouter.ai/api/v1"
     #LL_MODEL: Optional[str] = "openai/gpt-oss-safeguard-20b"
-    LL_MODEL: Optional[str] = "z-ai/glm-4.7-flash"
+    LL_MODEL: Optional[str] = "google/gemini-3-flash-preview"
     EMBEDDING_PROVIDER: Optional[str] = "ollama"
     EMBEDDING_API_KEY: Optional[str] = None
     EMBEDDING_BASE_URL: Optional[str] = None
